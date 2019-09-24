058a2210389590089fa2ff4364c230fe50467174
Piyush2769 <startcheckingyourmail@gmail.com>
2019-09-24 23:02:35 +0530

    Initial+1

--- app/src/main/java/com/piyushmaheswari/appointmentreminder/MainActivity.java
+++ app/src/main/java/com/piyushmaheswari/appointmentreminder/MainActivity.java
@@ -2,8 +2,13 @@ package com.piyushmaheswari.appointmentreminder;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RelativeLayout relativeLayout;
+    private LinearLayout linearLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
