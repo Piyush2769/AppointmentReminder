e5253ddd77aded412fde869bfa44cb1c278bbe99
Piyush2769 <startcheckingyourmail@gmail.com>
2019-09-29 10:49:41 +0530

    Initial+1

--- app/src/main/java/com/piyushmaheswari/appointmentreminder/MainActivity.java
+++ app/src/main/java/com/piyushmaheswari/appointmentreminder/MainActivity.java
@@ -1,9 +1,11 @@
 package com.piyushmaheswari.appointmentreminder;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +13,10 @@ public class MainActivity extends AppCompatActivity {
     private LinearLayout linearLayout;
 
     private RelativeLayout relativeLayout6;
+
+
+    private TextView ds;
+    private Image is;
     private LinearLayout linearLayout2;
 
     @Override
