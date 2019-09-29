ace4f504f7d3d1431e1ee435ae00b4912f0b9279
Piyush2769 <startcheckingyourmail@gmail.com>
2019-09-29 19:06:33 +0530

    Initial+1

---
+++ app/src/main/java/com/piyushmaheswari/appointmentreminder/AddAppointment.java
@@ -0,0 +1,19 @@
+package com.piyushmaheswari.appointmentreminder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class AddAppointment extends AppCompatActivity {
+
+
+    TextView ff;
+    ImageView kk;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_appointment);
+    }
+}
